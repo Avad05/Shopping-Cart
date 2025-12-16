c47be8a3ec8b4228f1ff24ff561bfd299bfa80b0
Avadhut <avadhutkaskar@gmail.com>
2025-12-16 15:54:53 +0530

    Single product page done

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext} from 'react';
 import { useCart } from '../CartContext/CartContext'
 import noty from "../pages/Shop.module.css"
 import { Link } from 'react-router-dom';
@@ -30,7 +30,7 @@ const Shop = () => {
         <div className={noty.shop}>
         {products.map((product) =>(
           
-          <Link to={`shop/${product.id}`} key={product.id} className={noty.card}>
+          <Link to={`${product.id}`} key={product.id} className={noty.card}>
             <div className={noty.imagCont} ><img src={product.image} alt='****' className={noty.image}></img></div>
             <div className='productDetails'>
               <h5 className={noty.productTitle}>{product.title}</h5>
--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useCart } from "../CartContext/CartContext";
+import styles from "../pages/SingleProduct.module.css";
 
 const SingleProduct = () => {
-  return (
-    <div>SingleProduct</div>
-  )
-}
+  const { id } = useParams();
+  const { addToCart } = useCart();
 
-export default SingleProduct
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [quantity, setQuantity] = useState(1);
+
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        setProduct(data);
+        setLoading(false);
+      })
+      .catch(err => console.error(err));
+  }, [id]);
+
+  if (loading) return <h1>Loading...</h1>;
+  if (!product) return <h2>No Product Found</h2>;
+
+  function increment() {
+    setQuantity(q => q + 1);
+  }
+
+  function decrement() {
+    setQuantity(q => (q > 1 ? q - 1 : 1));
+  }
+
+  return (
+    <>
+    <div className={styles.productCard}>
+      <img src={product.image} alt={product.title} className={styles.image} />
+
+      <div className={styles.productInfo}>
+        <h2>{product.title}</h2>
+        <h3>${product.price}</h3>
+
+        <div className={styles.add}>
+          <button onClick={decrement} className={styles.button}>−</button>
+          <span>{quantity}</span>
+          <button onClick={increment} className={styles.button}>+</button>
+        </div>
+
+        <button onClick={() => addToCart(product, quantity)}>
+          Add to Cart
+        </button>
+      </div>      
+    </div>
+      <div>
+     <h1>More About this Product</h1>
+      <p>{product.description}</p>
+      </div>
+      </>
+  );
+};
+
+export default SingleProduct;
